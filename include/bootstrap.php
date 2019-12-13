462447acc1719692efd0d22aae379a2e0fd205da
Mark <mrandall@digitellinc.com>
2019-12-13 19:29:15 +0000

    Fix for slashes in paths

--- include/bootstrap.php
+++ include/bootstrap.php
@@ -3,6 +3,8 @@
 	include_once __DIR__ . '/changelogs.inc';
 	
 	spl_autoload_register(static function($class_id) {
-		$absolute = __DIR__ . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, $class_id) . '.php';
-		require_once $absolute;
+		$absolute = realpath(__DIR__ . DIRECTORY_SEPARATOR . str_replace('/', DIRECTORY_SEPARATOR, $class_id) . '.php');
+		if ($absolute) {
+			require_once $absolute;
+		}
 	});
